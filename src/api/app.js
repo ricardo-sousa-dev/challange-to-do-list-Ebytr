3f8ee7ea81380f39037050da2b8aeb76ccaaf1cc
ricardo-sousa <rwmsousa@gmail.com>
2022-02-14 19:21:51 -0300

    corrige erros de requisição - cors

--- src/api/app.js
+++ src/api/app.js
@@ -2,9 +2,11 @@ const express = require('express'); // import express module
 require('dotenv').config(); // import dotenv to use .env file
 const bodyParser = require('body-parser'); // import body-parser to parse the body of the request
 const errorHandler = require('../middlewares/errorHandler'); // import errorHandler - middleware of error
+const router = require('../routes'); // import router from routes
+const cors = require('cors'); // import cors to allow cross-origin requests
 
 const app = express(); // create an instance of express
-const router = require('../routes'); // import router from routes
+app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // use body-parser to parse the body of the request
